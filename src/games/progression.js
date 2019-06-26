3fd723515d9cadf4bbc24a153af930a141de50e9
Shlyapnikov <anshlyapnikov@gmail.com>
2019-06-26 23:11:42 +0300

    refactoring brain-progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,21 +5,22 @@ const description = 'What number is missing in the progression?';
 const length = 10;
 
 const dataFlow = () => {
-  const positionHiddenElement = generateNumber(0, length - 1);
+  const hiddenElementPosition = generateNumber(0, length - 1);
   const startElement = generateNumber(1, 50);
   const stepOfPrpgression = generateNumber(1, 4);
 
-  const rightAnswer = `${startElement + stepOfPrpgression * positionHiddenElement}`;
+  const rightAnswer = `${startElement + stepOfPrpgression * hiddenElementPosition}`;
   const progression = [];
   let question = '';
 
   for (let i = 0; i < length; i += 1) {
-    progression.push(startElement + stepOfPrpgression * i);
-    if (i === positionHiddenElement) {
-      progression[positionHiddenElement] = '..';
+    if (i !== hiddenElementPosition) {
+      progression.push(startElement + stepOfPrpgression * i);
+    } else {
+      progression[hiddenElementPosition] = '..';
     }
-    question = progression.join(' ');
   }
+  question = progression.join(' ');
   return [question, rightAnswer];
 };
 
